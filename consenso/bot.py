a20124ee04e48da6739344628751b65cee8a2731
Danny O'Brien <Danny O'Brien danny@spesh.com>
2012-08-05 00:17:40 -0700

    Initial commit of blank code

---
+++ consenso/bot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+##
+# bot.py
+###
+"""bot.py
+
+"""
+
+__author__ = "Danny O'Brien <http://www.spesh.com/danny/>"
+__copyright__ = "Copyright Danny O'Brien"
+__contributors__ = None
+__license__ = "GPL v3"
+
+
+def main(args):
+    print("called with {}".format(args))
