307a6443b9b1c12613984e8201dce14e18031519
Danny O'Brien <Danny O'Brien danny@spesh.com>
2012-08-05 13:12:27 -0700

    Skeleton of command line handling

--- consenso/bot.py
+++ consenso/bot.py
@@ -2,8 +2,9 @@
 ##
 # bot.py
 ###
-"""bot.py
+"""consensobot
 
+Runs a consensobot on your choice of IRC network.
 """
 
 __author__ = "Danny O'Brien <http://www.spesh.com/danny/>"
@@ -11,6 +12,13 @@ __copyright__ = "Copyright Danny O'Brien"
 __contributors__ = None
 __license__ = "GPL v3"
 
+import argparse
+from distutils2.database import get_distribution
+metadata = get_distribution('consensobot').metadata
 
-def main(args):
-    print("called with {}".format(args))
+
+def main():
+    parser = argparse.ArgumentParser(description=metadata.get('summary'),
+            epilog="Mail {} <{}> with bugs and features.".format(metadata.get('maintainer'),
+                metadata.get('maintainer_email')))
+    parser.parse_args()
