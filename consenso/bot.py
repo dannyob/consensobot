617d2b7c069fdec2aa002a6ede56ed24e7c0b8c4
Danny O'Brien <Danny O'Brien danny@spesh.com>
2012-08-05 17:17:14 -0700

    Initial argument passing for add_text

--- consenso/bot.py
+++ consenso/bot.py
@@ -12,13 +12,33 @@ __copyright__ = "Copyright Danny O'Brien"
 __contributors__ = None
 __license__ = "GPL v3"
 
+import sys
 import argparse
 from distutils2.database import get_distribution
 metadata = get_distribution('consensobot').metadata
 
 
-def main():
+class Corpus():
+    def __init__(self, location=None):
+        self.texts = []
+        return
+
+    def clear(self):
+        self.texts = []
+
+
+def command_add_text(parsed_args):
+    print("I have learned {}".format(parsed_args.location.name))
+
+
+def main(args=sys.argv):
     parser = argparse.ArgumentParser(description=metadata.get('summary'),
             epilog="Mail {} <{}> with bugs and features.".format(metadata.get('maintainer'),
                 metadata.get('maintainer_email')))
-    parser.parse_args()
+    subparsers = parser.add_subparsers()
+    parser_add_text = subparsers.add_parser('add_text',
+            help='Adds a text file to corpus of Consenso markov knowledge.')
+    parser_add_text.add_argument('location', type=argparse.FileType())
+    parser_add_text.set_defaults(func=command_add_text)
+    parsed_args = parser.parse_args(args)
+    parsed_args.func(parsed_args)
