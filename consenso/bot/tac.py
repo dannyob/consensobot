444517e3ea9564857c8ffec736bb3af69af15535
Danny O'Brien <Danny O'Brien dobrien@cpj.org>
2012-08-15 18:56:19 -0700

    Switch to Foolscap

--- consenso/bot/tac.py
+++ consenso/bot/tac.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 ##
-# ircbot.tac
+# tac.py
 ###
-"""ircbot.tac
+"""tac.py
 
-Twisted Application .tac file
+Twisted Application .tac file for the Consenso process.
+
 See <http://twistedmatrix.com/documents/current/core/howto/application.html>
 """
 
@@ -13,39 +14,25 @@ __copyright__ = "Copyright Danny O'Brien"
 __contributors__ = None
 __license__ = "GPL v3"
 
-from twisted.words.protocols import irc
-from twisted.internet import protocol
+import os
+
 from twisted.application import service
+from foolscap.api import Referenceable, Tub
+
+import consenso.directories
 
 
-class ConsensoBot(irc.IRCClient):
-    def _get_nickname(self):
-        return self.factory.nickname
-    nickname = property(_get_nickname)
+class RemoteControl(Referenceable):
+    def remote_irc_joint(self, host, port, group=None, nick=None):
+        print "I am trying to join {} on {}:{} as {}".format(group, host, port, nick)
 
-    def signedOn(self):
-        self.join(self.factory.channel)
-        print "Signed on as %s." % (self.nickname,)
-
-    def joined(self, channel):
-        print "Joined %s." % (channel,)
-
-    def privmsg(self, user, channel, msg):
-        print msg
-
-
-class ConsensoBotFactory(protocol.ClientFactory):
-    protocol = ConsensoBot
-
-    def __init__(self, channel, nickname='ConsensoSimple'):
-        self.channel = channel
-        self.nickname = nickname
-
-    def clientConnectionLost(self, connector, reason):
-        print "Lost connection (%s), reconnecting." % (reason,)
-        connector.connect()
-
-    def clientConnectionFailed(self, connector, reason):
-        print "Could not connect: %s" % (reason,)
+myserver = RemoteControl()
+tub = Tub(certFile=os.path.join(consenso.directories.foolscap_dir, "pb2server.pem"))
+tub.listenOn("tcp:12345")
+tub.setLocation("localhost:12345")
+url = tub.registerReference(myserver, "remote-control")
+print("pid ", os.getpid())
+print("furl ", url)
 
 application = service.Application("Consenso Twisted Application")
+tub.setServiceParent(application)
